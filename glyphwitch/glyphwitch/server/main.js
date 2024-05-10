abf28e7fc663b069f9495f1cfd03ffa95ecaa71a
James Haner <jrhaner@memphis.edu>
2024-05-10 14:31:41 -0500

    more changes

--- glyphwitch/glyphwitch/server/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import './collections';
-
-Meteor.startup(() => {
-  // welcome message
-  console.log('*** GlyphWitch Server ***');
-  console.log('Welcome to the GlyphWitch server!');
-});
